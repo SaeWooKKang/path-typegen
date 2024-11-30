8c5a9ec2aa6de46c1cecf3305a0965b57247e097
saewookkang <pjy9665@kakao.com>
2024-11-30 15:58:29 +0900

    feat(getAllFiles.ts): Create a function to get all file names

---
+++ src/__internal__/__tests__/getAllFiles.test.ts
@@ -0,0 +1,18 @@
+import { expect, it, describe } from "vitest";
+import { getAllFiles } from "../getAllFiles.js";
+
+describe("getAllFiles", () => {
+  it("one file", () => {
+    const DIRECTORY_PATH = './src/__internal__/__tests__/__assets__/bar'
+    const res = getAllFiles(DIRECTORY_PATH)
+  
+    expect(res[0]).toBe(`${DIRECTORY_PATH}/index.ts`)
+  })
+
+  it("recursive files", () => {
+    const DIRECTORY_PATH = './src/__internal__/__tests__/__assets__'
+    const res = getAllFiles(DIRECTORY_PATH)
+  
+    expect(res.length).toBe(3)
+  })
+})
