1b18178b6050344faf0aa3ae291531034f90ec78
saewookkang <pjy9665@kakao.com>
2024-11-30 16:04:24 +0900

    chore: format all files with biome

--- src/__internal__/__tests__/getAllFiles.test.ts
+++ src/__internal__/__tests__/getAllFiles.test.ts
@@ -1,18 +1,18 @@
-import { expect, it, describe } from "vitest";
-import { getAllFiles } from "../getAllFiles.js";
+import { describe, expect, it } from 'vitest';
+import { getAllFiles } from '../getAllFiles.js';
 
-describe("getAllFiles", () => {
-  it("one file", () => {
-    const DIRECTORY_PATH = './src/__internal__/__tests__/__assets__/bar'
-    const res = getAllFiles(DIRECTORY_PATH)
-  
-    expect(res[0]).toBe(`${DIRECTORY_PATH}/index.ts`)
-  })
+describe('getAllFiles', () => {
+	it('one file', () => {
+		const DIRECTORY_PATH = './src/__internal__/__tests__/__assets__/bar';
+		const res = getAllFiles(DIRECTORY_PATH);
 
-  it("recursive files", () => {
-    const DIRECTORY_PATH = './src/__internal__/__tests__/__assets__'
-    const res = getAllFiles(DIRECTORY_PATH)
-  
-    expect(res.length).toBe(3)
-  })
-})
+		expect(res[0]).toBe(`${DIRECTORY_PATH}/index.ts`);
+	});
+
+	it('recursive files', () => {
+		const DIRECTORY_PATH = './src/__internal__/__tests__/__assets__';
+		const res = getAllFiles(DIRECTORY_PATH);
+
+		expect(res.length).toBe(3);
+	});
+});
--- src/__internal__/getAllFiles.ts
+++ src/__internal__/getAllFiles.ts
@@ -1,21 +1,21 @@
-import fs from "node:fs";
+import fs from 'node:fs';
 
 export const getAllFiles = (path: string) => {
-  let files: string[] = [];
+	let files: string[] = [];
 
-  const entries = fs.readdirSync(path, { withFileTypes: true });
+	const entries = fs.readdirSync(path, { withFileTypes: true });
 
-  for (const entry of entries) {
-    const fullPath = path.concat('/', entry.name);
+	for (const entry of entries) {
+		const fullPath = path.concat('/', entry.name);
 
-    if (entry.isDirectory()) {
-      const subFiles = getAllFiles(fullPath);
+		if (entry.isDirectory()) {
+			const subFiles = getAllFiles(fullPath);
 
-      files = [...files, ...subFiles];
-    } else {
-      files.push(fullPath);
-    }
-  }
-  
-  return files;
-}
+			files = [...files, ...subFiles];
+		} else {
+			files.push(fullPath);
+		}
+	}
+
+	return files;
+};
