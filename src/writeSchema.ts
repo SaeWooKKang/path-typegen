d651b8155002efe47611715c58901b691d7aff3c
saewookkang <pjy9665@kakao.com>
2024-12-02 15:37:13 +0900

    feat(writeSchema): Add missing options

--- src/writeSchema.ts
+++ src/writeSchema.ts
@@ -1,9 +1,14 @@
 import fs from 'node:fs';
 import { getAllFiles } from './__internal__/getAllFiles';
-import { pathToSchema } from './__internal__/pathToSchema';
+import { pathToSchema, type Options } from './__internal__/pathToSchema';
 
-export const writeSchema = async (inputPath: string, outputPath: string) => {
-	const schema = pathToSchema(getAllFiles(inputPath));
+export const writeSchema = async (
+	inputPath: string,
+	outputPath: string,
+	options: Options,
+) => {
+	const allFiles = getAllFiles(inputPath);
+	const schema = pathToSchema(allFiles, options);
 
 	await fs.promises.writeFile(outputPath, schema.toJSON());
 };
