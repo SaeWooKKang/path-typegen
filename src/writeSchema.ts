ded8346c9a3b4989862ca89b04a7c8cf773c04d9
saewookkang <pjy9665@kakao.com>
2024-12-02 16:32:10 +0900

    fix(writeSchema): change options to optional

--- src/writeSchema.ts
+++ src/writeSchema.ts
@@ -1,14 +1,14 @@
 import fs from 'node:fs';
 import { getAllFiles } from './__internal__/getAllFiles';
-import { pathToSchema, type Options } from './__internal__/pathToSchema';
+import { type Options, pathToSchema } from './__internal__/pathToSchema';
 
 export const writeSchema = async (
-	inputPath: string,
-	outputPath: string,
-	options: Options,
+  inputPath: string,
+  outputPath: string,
+  options?: Options,
 ) => {
-	const allFiles = getAllFiles(inputPath);
-	const schema = pathToSchema(allFiles, options);
+  const allFiles = getAllFiles(inputPath);
+  const schema = pathToSchema(allFiles, options);
 
-	await fs.promises.writeFile(outputPath, schema.toJSON());
+  await fs.promises.writeFile(outputPath, schema.toJSON());
 };
