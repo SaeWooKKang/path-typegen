6808d540ebb0816b4342ae8d7b524cdd401a073f
saewookkang <pjy9665@kakao.com>
2025-02-17 17:19:38 +0900

    fix(ph.ts): change export default to named export for usability

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { ph } from './ph';
+export { ph } from './ph';
 
 export { typed } from './typed';
 export type { PathGen } from './__internal__/Ph';
 export type { CLIOptions } from './runCLI';
-
-export default ph;
